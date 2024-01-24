6b394c81bb57f34216714d624281b32cfc846f53
Yurii <131679817+havr1985@users.noreply.github.com>
2024-01-24 09:52:20 +0200

    init

---
+++ my-app/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react"
+
+
+
+interface IDashboardPageProps {
+
+}
+
+const DashboardPage:FC<IDashboardPageProps> = () => {
+    return (
+        <div>Dashboard</div>
+    )
+}
+
+export default DashboardPage;
